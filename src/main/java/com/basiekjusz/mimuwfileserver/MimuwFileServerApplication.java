dda524f4e96fc4cec6c9a61b1480a078607b8f62
Jasiek Busz <jasiekbusz@gmail.com>
2021-05-25 11:35:10 +0200

    Initial commit

---
+++ src/main/java/com/basiekjusz/mimuwfileserver/MimuwFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.basiekjusz.mimuwfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimuwFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimuwFileServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basiekjusz/mimuwfileserver/MimuwFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basiekjusz.mimuwfileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimuwFileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
